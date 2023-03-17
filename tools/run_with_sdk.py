7e8129245cd7b4fa59a89e2b0a9170c8a166e92c
Kyan Chen <keyan1055002755@163.com>
2023-03-17 23:53:01 +0800

    add

--- tools/run_with_sdk.py
+++ tools/run_with_sdk.py
@@ -38,11 +38,11 @@ def main():
     segmentor = Segmentor(
         model_path=args.model_path, device_name=args.device_name, device_id=0)
     time_t = 0
-    for i in range(20):
+    for i in range(10):
         t_s = time.time()
         seg = segmentor(img)
         time_t += time.time() - t_s
-    print(time_t / 20)
+    print(time_t / 10)
 
     if seg.dtype == np.float32:
         seg = np.argmax(seg, axis=0)
